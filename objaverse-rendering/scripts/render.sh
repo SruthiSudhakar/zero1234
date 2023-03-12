e8b3d17395ad14fd353c674d448df93bc86086a1
ruoshiliu <rl3111@columbia.edu>
2023-03-11 21:58:21 -0500

    updates

--- objaverse-rendering/scripts/render.sh
+++ objaverse-rendering/scripts/render.sh
@@ -1,5 +1,4 @@
-cd /home/rliu/Desktop/cvfiler04/ruoshi/github/objaverse-rendering
 python scripts/distributed.py \
 	--num_gpus 8 \
 	--workers_per_gpu 2 \
-	--input_models_path /home/rliu/.objaverse/hf-objaverse-v1/object-paths.json
+	--input_models_path .objaverse/hf-objaverse-v1/object-paths.json
